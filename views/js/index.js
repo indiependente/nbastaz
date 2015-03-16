074d732657fb0ac2a6315614ddbc366f092526fa
weareback <carter91@libero.it>
2015-03-16 23:07:01 +0100

    Views:added standing table for Eastern conference

--- views/js/index.js
+++ views/js/index.js
@@ -1,13 +1,15 @@
 var app = angular.module('nbastaz', ['ngMaterial']);
 
+
  app.controller('nextMatchController', function($scope, $http){
         var todayAll = Date.today();
         var month = todayAll.getMonth()+1;
         var day = todayAll.toFormat('DD');
         var year = todayAll.getFullYear();
         var today = month+'-'+day+'-'+year;
+        $scope.nm=false;
         $http.get("http://localhost:3000/matches?date="+today)
-        .success(function(response) {$scope.results = response;});
+        .success(function(response) {$scope.results = response;$scope.nm=true;});
     });
 
     app.controller('prevMatchController', function($scope, $http){
@@ -16,8 +18,15 @@ var app = angular.module('nbastaz', ['ngMaterial']);
         var day = yesterdayAll.toFormat('DD');
         var year = yesterdayAll.getFullYear();
         var yesterday = month+'-'+day+'-'+year;
+        $scope.pm=false;
         $http.get("http://localhost:3000/matches?date="+yesterday)
-        .success(function(response) {$scope.results = response;});
+        .success(function(response) {$scope.results = response;$scope.pm=true;});
+    });
+
+
+    app.controller('standingsController',function($scope,$http){
+        $http.get("http://localhost:3000/standings")
+        .success(function(response){$scope.results=response;$scope.erange=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14];$scope.wrange=[15,16,17,18,19,20,21,22,23,24,25,26,27,28,29];})
     });
 
 
@@ -57,18 +66,22 @@ app.controller('AppCtrl', ['$scope', '$mdSidenav', 'pageService', '$timeout','$l
 app.service('pageService', ['$q', function($q) {
   var pages = [{
       name: 'Home',
+      // iconurl: 'http://upload.wikimedia.org/wikipedia/en/thumb/0/07/NBALogo.svg/197px-NBALogo.svg.png',
       href: '/home.html'
   }, {
       name: 'Teams',
+      //iconurl: 'http://www.jamesrobertwatson.com/images/Thunderlogo/nbalogos.jpg',
       href: '/teams.html'
   }, {
       name: 'Players',
+      // iconurl:'http://www.killthecablebill.com/images/cancelcable/2014/12/derrick-rose-nba-chicago-bulls-basketball-slam-dunk-stuck-shape-white-background-ball.jpg',
       href: '/players.html'
   }, {
       name: 'Highlights',
       href: '/highlights.html'
   }, {
       name: 'Standings',
+      // iconurl: 'http://www.scoresreport.com/wp-content/uploads/2010/04/nba-playoffs-bracket.jpg',
       href: '/standings.html'
   }];
 
