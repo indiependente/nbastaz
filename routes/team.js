84d2237bd5c8618886b60f61f17fe520c232ff4d
Francesco Farina <rockerg991@gmail.com>
2015-03-17 19:11:10 +0100

    routest/team.js: team info integration. Needs more work.

--- routes/team.js
+++ routes/team.js
@@ -2,9 +2,25 @@ var express     = require('express'),
     router      = express.Router(),
     team 		= require('../bin/espn_team')
 
-router.get('/', function (req, res) {
+router.get('/stats', function (req, res) {
   res.type('json')
-  team(res)
+  // parse the request parameters and get the url
+  var url = '' // to be filled
+  team.teamStats(url, res)
+})
+
+router.get('/leaders', function (req, res) {
+  res.type('json')
+  // parse the request parameters and get the url
+  var url = '' // to be filled
+  team.teamLeaders(url, res)
+})
+
+router.get('/roster', function (req, res) {
+  res.type('json')
+  // parse the request parameters and get the url
+  var url = '' // to be filled
+  team.teamRoster(url, res)
 })
 
 module.exports = router
