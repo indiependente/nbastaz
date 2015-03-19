cbd17ed6b5af2e555115e74ed8cc51847ff7ffae
weareback <carter91@libero.it>
2015-03-19 19:50:38 +0100

    Views:team.html: added temporary grid for teams visualization

--- views/js/index.js
+++ views/js/index.js
@@ -9,7 +9,7 @@ var app = angular.module('nbastaz', ['ngMaterial']);
         var today = month+'-'+day+'-'+year;
         $scope.nm=false;
         $scope.today = today;
-        $http.get("http://localhost:3000/matches?date="+today)
+        $http.get("/matches?date="+today)
         .success(function(response) {$scope.results = response;$scope.nm=true;});
     });
 
@@ -21,7 +21,7 @@ var app = angular.module('nbastaz', ['ngMaterial']);
         var yesterday = month+'-'+day+'-'+year;
         $scope.pm=false;
         $scope.yesterday = yesterday;
-        $http.get("http://localhost:3000/matches?date="+yesterday)
+        $http.get("/matches?date="+yesterday)
         .success(function(response) {$scope.results = response;$scope.pm=true;});
     });
 
@@ -29,11 +29,72 @@ var app = angular.module('nbastaz', ['ngMaterial']);
     app.controller('standingsController',function($scope,$http){
         $scope.erange=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14];
         $scope.wrange=[15,16,17,18,19,20,21,22,23,24,25,26,27,28,29];
-        $http.get("http://localhost:3000/standings")
+        $http.get("/standings")
         .success(function(response){
           $scope.results=response;
         })
     });
+
+
+
+    /*********GRID LIST*******/
+
+    app.controller('gridList', function($scope,$http) {
+      $http.get("/teams")
+        .success(function(response){
+          $scope.res=response;
+
+        })
+
+      this.tiles = buildGridModel({
+            icon : "avatar:svg-",
+            title: "Svg-",
+            background: ""
+          });
+
+    function buildGridModel(tileTmpl){
+      var it, results = [ ];
+      for (var j=0; j<30; j++) {
+        it = angular.extend({},tileTmpl);
+        
+        console.log($scope.res);
+        
+        it.icon  = $scope.res[j][0];
+        it.title = it.title + (j+1);
+        it.span  = { row : "1", col : "1" };
+        switch(j+1) {
+          case 1:
+            it.background = "red";
+            it.span.row = it.span.col = 2;
+            break;
+          case 2: it.background = "green";         break;
+          case 3: it.background = "darkBlue";      break;
+          case 4:
+            it.background = "blue";
+            it.span.col = 2;
+            break;
+          case 5:
+            it.background = "yellow";
+            it.span.row = it.span.col = 2;
+            break;
+          case 6: it.background = "pink";          break;
+          case 7: it.background = "darkBlue";      break;
+          case 8: it.background = "purple";        break;
+          case 9: it.background = "deepBlue";      break;
+          case 10: it.background = "lightPurple";  break;
+          case 11: it.background = "yellow";       break;
+        }
+        results.push(it);
+      }
+      return results;
+    }
+  })
+  .config( function( $mdIconProvider ){
+    $mdIconProvider.iconSet("avatar", './icons/avatar-icons.svg', 128);
+  });
+
+
+  /*********GRID LIST*******/
 
 
 
